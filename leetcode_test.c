8bcabe112f645d0baaf8e663dcd3c0dfc2093801
kyh-ly <kyh1511226779@163.com>
2020-08-10 22:03:51 +0800

    add the count binary string function

--- leetcode_test.c
+++ leetcode_test.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int countBinarySubstirngs(char *s){
+	int n=0,pre=0,curr=1,len=strlen(s)-1;
+	for(int i=0;i<len;i++){
+		if(s[i] == s[i+1]) ++curr;
+		else{
+			pre = curr;
+			curr=1;
+		}
+		if(pre >= curr) ++n;
+	}
+	return n;
+}
